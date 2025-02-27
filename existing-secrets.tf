bef75161383bc9e9c43910088abeeaeb94832370
Boite <boite@luscii.com>
2025-02-27 11:45:18 +0100

    Implemented a workaround for the sensitive existing keys as well

--- existing-secrets.tf
+++ existing-secrets.tf
@@ -3,10 +3,12 @@ locals {
     for key, value in var.secrets : key => value.value_from_arn
     if contains(keys(value), "value_from_arn")
   }
+
+  existing_secrets_keys_set = toset(keys(local.existing_secrets))
 }
 
 data "aws_secretsmanager_secret" "existing" {
-  for_each = local.existing_secrets
+  for_each = local.existing_secrets_keys_set
 
-  arn = each.value
+  arn = local.existing_secrets[each.key]
 }
